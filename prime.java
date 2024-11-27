7f6203481ce49f9618627b8fe0eee58cabe4397f
22H51AO538GOPIKA <mgopika1703@gmail.com>
2024-11-27 18:06:13 +0530

    c5

--- prime.java
+++ prime.java
@@ -1,9 +1,10 @@
 public class prime{
     public static void main(String[] args){
-        System.out.println("prime new commit 1");
+        // System.out.println("prime new commit 1");
          System.out.println("prime new commit 1");
           System.out.println("prime new commit 2");
            System.out.println("prime new commit 3");
             System.out.println("prime new commit 4");
+             System.out.println("prime new commit 5");
     }
 }
